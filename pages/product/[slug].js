a389b100dd2a68c76a636259ef51e40f74bbb7fd
Forever Young <dy9040@gmail.com>
2022-08-07 16:28:33 -0500

    added cart funcationalilty

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,10 +3,12 @@ import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
 
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
     const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty, onAdd } = useStateContext();
     return (
         <div>
             <div className="product-detail-container">
@@ -46,17 +48,17 @@ const ProductDetails = ({ product, products }) => {
                         <h3>Quantity:</h3>
                         <p className="quantity-desc">
                             <span className="minus" 
-                            onClick=""><AiOutlineMinus /></span>
+                            onClick={decQty}><AiOutlineMinus /></span>
                             <span className="num" 
-                            onClick="">0</span>
+                            onClick="">{qty}</span>
                             <span className="plus" 
-                            onClick=""><AiOutlinePlus /></span>
+                            onClick={incQty}><AiOutlinePlus /></span>
                         </p>
                     </div>
                     <div className="buttons">
                         <button type="button" 
                         className="add-to-cart" 
-                        onClick="">Add to cart</button>
+                        onClick={() => onAdd(product, qty)}>Add to cart</button>
                         <button type="button" className="buy-now" onClick="">Buy Now</button>
                     </div>
                 </div>
